9d4e45ac865cf5f866ea940053b77d32987130f7
tanerjn <stanermetin@gmail.com>
2022-10-23 16:58:52 +0200

    update notes-3.txt

--- devops/update.sh
+++ devops/update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for x in 1 2 .. 3
+for x in 1 2 .. 2
 do
 		echo "update" >> notes-3.txt
 done
 
 
-for x in 1 2 .. 3
+for x in 1 2 .. 2
 do
 		sudo git add .
 		sudo git commit -m "update notes-3.txt"
